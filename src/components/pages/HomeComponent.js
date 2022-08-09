c0fd4012b67f25db30c76a1d7f630f747362529c
Vitto Albani <vdalbani-rodriguez@myseneca.ca>
2022-08-09 15:24:45 -0400

    banner changes

--- src/components/pages/HomeComponent.js
+++ src/components/pages/HomeComponent.js
@@ -6,11 +6,21 @@ export class HomeComponent extends Component {
 
   render() {
     return (
-    <section className='section'>
-      <Card>
-        <Card.Body>Home description Here</Card.Body>
-      </Card>
-    </section>
+    <>
+  <section className="hero">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-8">
+          <br/>
+          <br/>
+          <h1 className="title_1"><strong>Victor's Portfolio</strong></h1>
+          <h3 className="title_2">A blog to showcase my personal projects</h3>
+          <p className="discover">Discover More</p>
+        </div>
+      </div><a href="#" className="continue link-scroll"><i className="fa fa-long-arrow-down"></i> Scroll Down</a>
+    </div>
+  </section>
+    </>
     )
   }
 }
